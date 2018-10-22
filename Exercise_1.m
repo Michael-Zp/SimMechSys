02aa816eafe480551833e20e6d64bbcec23dd8e7
Michael Zappe <mz-171742@AP053108>
2018-10-22 13:31:14 +0200

    Advanced in exercise 1

--- Exercise_1.m
+++ Exercise_1.m
@@ -1,5 +1,8 @@
 clc
 
+%% rest
+
+%{
 a = [2; 0; 5];
 b = [4; 2; 1];
 c = [0; 1; 0];
@@ -12,7 +15,7 @@ B = [2 4 0;...
     3 2 0;...
     6 2 0];
 
-fprintf('%s\n', "A)")
+fprintf('%s\n', "a)")
 
 disp(a * transpose(b))
 disp(a + b)
@@ -24,12 +27,12 @@ disp(inv(A))
 disp(inv(B))
 
 
-fprintf('%s\n', "B)")
+fprintf('%s\n', "b)")
 
 fprintf('%s\n\n', "Was on a workshop, I dont have any exercises yet")
 
 
-fprintf('%s\n', "C)")
+fprintf('%s\n', "c)")
 
 steps = 0:0.05:2*pi;
 sinValues = sin(steps);
@@ -41,10 +44,103 @@ arctanValues = atan(steps);
 plot(steps, sinValues, "r",...
      steps, cosValues, "g",...
      steps, tanValues, "b")
-
+ 
 title("PlotsPlots")
+axis([0 2*pi -2 2])
 xlabel("Step")
 ylabel("MyValues")
 
-%plot(steps, arcsinValues, "c")
-%plot(steps, arctanValues, "m")
+subplot(2, 1, 1)
+
+plot(steps, arcsinValues, "c",...
+     steps, arctanValues, "m")
+
+
+title("ArcPlots")
+axis([0 2*pi -2 2])
+xlabel("Step")
+ylabel("YeahValues")
+%}
+
+
+%% bode plots
+%{
+fprintf('\n%s\n', "d)")
+
+K = [1, 1.5];
+T = [1, 5, 10];
+d = [0.5, 0.7, 1, 3];
+
+for i = 1:2
+    G1 = tf(K(i), [T(1), 1]);
+    G2 = tf(K(i), [T(1)^2 2*d(1)*T(1) 1]);
+    
+    figure(1);
+    bode(G1);grid on; hold on;
+    title("GPT1 K dynamic");
+    
+    figure(2);
+    bode(G2);grid on; hold on;
+    title("GPT2 K dynamic");
+end
+
+
+for i = 1:3
+    G1 = tf(K(1), [T(i), 1]);
+    G2 = tf(K(1), [T(i)^2 2*d(1)*T(i) 1]);
+    
+    figure(3);
+    bode(G1);grid on; hold on;
+    title("GPT1 T dynamic");
+    
+    figure(4);
+    bode(G2);grid on; hold on;
+    title("GPT2 T dynamic");
+end
+
+
+for i = 1:4
+    G = tf(K(1), [T(1)^2 2*d(i)*T(1) 1]);
+    
+    figure(5);
+    bode(G);grid on; hold on;
+    title("GPT2 d dynamic");
+end
+%}
+
+%% my bode
+
+[mag, phase] = mybode(1, [2 1], 0, 1);
+
+disp(mag)
+steps = 0:0.05:10;
+stepSize = size(steps);
+figure(1);
+plot(steps, real(phase(1, 1:stepSize(2))));
+
+sin([1 2])
+
+figure(2);
+bode(tf(1, [2, 1]))
+
+function [mag, phase] = mybode(a, b, Tt, w)
+    t1 = tf(a, b);
+    
+    freqThings = arrayfun(@(x) sin(x * w + Tt), 0:0.05:10);
+    
+    mag = 0;
+    phase = freqresp(t1, freqThings);
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
